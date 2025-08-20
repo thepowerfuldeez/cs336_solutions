c98b70c7ceb4e8fd262ae118371c6d4dbfffd571
thepowerfuldeez <deezchannel@gmail.com>
2025-08-20 23:45:47 +0100

    more efficient implementation of rope

--- assignment1-basics/cs336_basics/transformer/rope.py
+++ assignment1-basics/cs336_basics/transformer/rope.py
@@ -1,52 +1,57 @@
 """Rotary Positional Embeddings"""
 
+import time
+
 import torch
 import torch.nn as nn
 from torch import Tensor
 from jaxtyping import Float, Int
-from einops import einsum
+from einops import einsum, rearrange
+
+from cs336_basics.utils.logger import logger
 
 
-def get_cos_sin(m: int, theta_base: float, d_k: int, device) -> Float[Tensor, "d_k d_k"]:
+def get_cos_sin(
+    max_seq_len: int, theta_base: float, d_k: int, device
+) -> tuple[Float[Tensor, "max_seq d_k // 2"], Float[Tensor, "max_seq d_k // 2"]]:
     """
-    Make thetas matrix for rope
-
-    m - position
-    d_k - number of dimentions
+    Get cos and sin for every position
     """
     # for i = 1 (second sequence)
-    thetas = torch.tensor(theta_base).unsqueeze(0).repeat(d_k // 2)
-    to_pow = torch.tensor([-2 * (i - 1) / d_k for i in range(1, d_k // 2 + 1)])
-    thetas = m * thetas.pow(to_pow)
-
-    matrix = torch.zeros(d_k, d_k)
-    for k in range(1, d_k // 2 + 1):
-        x = torch.cos(thetas[k - 1])
-        y = torch.sin(thetas[k - 1])
-        matrix[2 * k - 2 : 2 * k - 1, 2 * k - 2 : 2 * k] = torch.tensor([x, -y])
-        matrix[2 * k - 1 : 2 * k, 2 * k - 2 : 2 * k] = torch.tensor([y, x])
-    return matrix
+    thetas = torch.tensor(theta_base, device=device).unsqueeze(0).repeat(d_k // 2)
+    j = torch.arange(0, d_k // 2, device=device)
+    inv_freqs: Float[Tensor, "d_k // 2"] = theta_base ** (-2 * j / d_k)
+    thetas: Float[Tensor, "max_seq_len, d_k // 2"] = torch.outer(torch.arange(max_seq_len, device=device), inv_freqs)
+    return thetas.cos(), thetas.sin()
 
 
 class RotatyPositionalEmbedding(nn.Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
         super().__init__()
-        full_rot_m = torch.zeros(max_seq_len, d_k, d_k, dtype=torch.float32, device=device)
-        for i in range(max_seq_len):
-            full_rot_m[i] = get_cos_sin(i, theta, d_k, device)
-        self.register_buffer("full_rot_m", full_rot_m, persistent=False)
+        t0 = time.monotonic()
+        cos, sin = get_cos_sin(max_seq_len, theta, d_k, device)
+        self.register_buffer("cos", cos, persistent=False)
+        self.register_buffer("sin", sin, persistent=False)
+        logger.info(f"RoPE initialized in {time.monotonic() - t0:.2}s.")
 
     def forward(
         self, x: Float[Tensor, "... seq_len d_k"], token_positions: Int[Tensor, "... seq_len"] | None = None
     ) -> Float[Tensor, "... seq_len d_k"]:
         in_dtype = x.dtype
         if token_positions is not None:
-            rot = self.full_rot_m[token_positions]
+            cos: Float[Tensor, "seq_len d_k // 2"] = self.cos[token_positions]
+            sin: Float[Tensor, "seq_len d_k // 2"] = self.sin[token_positions]
         else:
-            rot = self.full_rot_m[: x.size(-2)]
-        rotated: Float[Tensor, "... seq_len d_k"] = einsum(
-            x.to(torch.float32), rot, "... seq_len d1, ... seq_len d2 d1 -> ... seq_len d2"
-        )
+            cos: Float[Tensor, "seq_len d_k // 2"] = self.cos[: x.size(-2)]
+            sin: Float[Tensor, "seq_len d_k // 2"] = self.sin[: x.size(-2)]
+
+        x_pairs = rearrange(x.to(torch.float32), "... seq_len (d_k_half t) -> ... seq_len d_k_half t", t=2)
+        x1, x2 = x_pairs[..., 0], x_pairs[..., 1]
+
+        row1 = x1 * cos - x2 * sin
+        row2 = x1 * sin + x2 * cos
+        rotated = torch.stack([row1, row2], dim=-1)
+        rotated = rearrange(rotated, "... seq_len d_k_half t -> ... seq_len (d_k_half t)", t=2)
         return rotated.to(in_dtype)
 
 
