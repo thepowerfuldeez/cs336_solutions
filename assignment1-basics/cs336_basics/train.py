2f00fe3c0d4cbf18afe73844a612eff389a7aa8a
thepowerfuldeez <deezchannel@gmail.com>
2025-09-01 22:29:56 +0100

    set latest config by default

--- assignment1-basics/cs336_basics/train.py
+++ assignment1-basics/cs336_basics/train.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import wandb
 
+from cs336_basics.configs.gpt_small_faster import cfg
 from cs336_basics.config_schema import Config
 from cs336_basics.utils.config_tools import dataclass_to_nested_dict, wandb_run_name
 from cs336_basics.training.trainer import Trainer
@@ -62,9 +63,6 @@ def test(cfg: Config):
 
     trainer = Trainer(cfg)
     print(trainer.generate(torch.tensor([0, 1, 2]), 5, top_p=0.8, temperature=0.1))
-
-
-from cs336_basics.configs.gpt_small import cfg
 
 train(cfg, parse_args())
 # test(cfg)
